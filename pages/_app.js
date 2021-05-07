184e45377c5891c5d8a95ee04d50434b31930aff
Jesus Ramirez <jrmz.maldonado@gmail.com>
2021-05-06 21:25:32 -0500

    Modify _app.js file to add initialprops

--- pages/_app.js
+++ pages/_app.js
@@ -3,3 +3,14 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
+
+// Esta función va y busca props y data de cualquier componente dentro de MyApp
+// Si alguna otra página tiene getInitialProps on them, entonces vamos a esperar y fetch la información
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query; // get any query variables
+  return { pageProps };
+};
